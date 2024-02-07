9d53fcf3cf02725dd3f1ce654bdc59376a21c3a3
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-02-07 19:07:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
+import os
+
 API_ID = 11573285
 API_HASH = "f2cc3fdc32197c8fbaae9d0bf69d2033"
 BOT_TOKEN = "6122169010:AAEavMTJpHfLfsEEuuLXK-sjWzzjP9Y6uJA"
+REDIS_URI = None
+REDIS_PASSWORD = None
+REDISPASSWORD = os.environ.get("REDISPASSWORD", None)
+REDISHOST = os.environ.get("REDISHOST", None)
+REDISUSER = os.environ.get("REDISUSER", None)
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
+MONGO_URI = os.environ.get("MONGO_URI", None)
